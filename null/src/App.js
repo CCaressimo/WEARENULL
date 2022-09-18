de0690ff309b02ad3561f7c34346154055a0b1e0
CCaressimo <c.caressimo@gmail.com>
2022-09-18 01:20:14 -0400

    temp v1

---
+++ null/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Halo from './components/Halo';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Halo />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
