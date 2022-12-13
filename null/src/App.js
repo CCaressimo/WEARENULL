062d28f5276ce3a1cf9346aa8f143c16c0df4603
CCaressimo <c.caressimo@gmail.com>
2022-12-12 22:41:40 -0500

    fix warnings

--- null/src/App.js
+++ null/src/App.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Splash from "./components/Splash";
-import linkTree from "./components/linkTree"
 
 const App = () => {
 
@@ -10,7 +9,6 @@ const App = () => {
     <Router>
       <Routes>
         <Route element={<Splash/>} path="/" exact />
-        <Route element={<linkTree/>} path="/linktree" />
       </Routes>
     </Router>
   );
