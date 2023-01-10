1c6354b23fccc43ac1ed4c9ec10399656a69883b
CCaressimo <c.caressimo@gmail.com>
2023-01-10 00:28:45 -0500

    added merch url

--- null/src/components/Splash.jsx
+++ null/src/components/Splash.jsx
@@ -1,6 +1,4 @@
 import React, {useState} from 'react'
-import MailchimpSubscribe from "react-mailchimp-subscribe";
-import CustomForm from './CustomForm';
 import { FaFacebookSquare, FaInstagram, FaTwitterSquare, FaSpotify, FaTiktok, FaYoutube} from 'react-icons/fa'
 import { SiApplemusic } from "react-icons/si";
 import  "./css/Splash.css"
@@ -8,32 +6,8 @@ import halo from "../assets/haloSplash.mp4"
 import nullLogo from "../assets/NullwhiteLogo.png"
 import nullSigilRed from "../assets/nullSigilRed.png"
 import sigil from "../assets/Sigilwhite.png"
-import Modal from 'react-modal'
 
-const {REACT_APP_U, REACT_APP_ID} = process.env
-
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: 'black'
-  },
-};
-
-const Splash = props => {
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const openModal = () => {
-    setIsOpen(true)
-  }
-  const closeModal = () => {
-    setIsOpen(false)
-  }
-
-  const postUrl = `https://Wearenull.us12.list-manage.com/subscribe/post?u=${REACT_APP_U}&id=${REACT_APP_ID}`;
+const Splash = () => {
   return (
     <>
       <main>
@@ -60,50 +34,23 @@ const Splash = props => {
               </div>
               <div className=''>
                 <div className="circle flex justify-center items-center " onMouseOver='circle'>
-                  <button onClick={openModal}>
-                    <img src={sigil} 
-                      alt="sigil" 
-                      className="sigil"
-                    />
-                    <div className="environment"></div>
-                    <h2 className='enterText headerText hero glitch layers flex justify-center items-center' data-text="HELP ME">
-                      <span>ENTER</span>
-                    </h2>
-                  </button>
+                  <a href="https://null-merch-store.myshopify.com">
+                    <button>
+                      <img src={sigil} 
+                        alt="sigil" 
+                        className="sigil"
+                      />
+                      <div className="environment"></div>
+                      <h2 className='enterText headerText hero glitch layers flex justify-center items-center' data-text="HELP ME">
+                        <span>SHOP NOW</span>
+                      </h2>
+                    </button>
+                  </a>
                 </div>
               </div>
             </div>
           </section>
-      </main>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Example Modal"
-        style={customStyles}
-        >
-        <div className='modalOuter'>
-          <div className='modalInner'>
-            <img src={nullSigilRed} alt="Null logo" className="sigilModal"/>
-          </div>
-          
-          <div>
-            <h1 className='headerText'>JOIN THE CULT</h1>
-          </div>
-        </div>
-        <div className='modalOuter'>
-          <MailchimpSubscribe 
-            url={postUrl}
-            render={({ subscribe, status, message }) => (
-                    <CustomForm
-                        status={status} 
-                        message={message}
-                        onValidated={formData => subscribe(formData)}
-                        closeModal={closeModal}
-                    />
-                )}
-          />
-        </div>
-      </Modal>      
+      </main>   
     </>
   )
 }
